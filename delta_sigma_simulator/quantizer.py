eecb48dadbcab09be39b12fc1888ccb2f852dfa0
Caro Meysmans <caro.meysmans@gmail.com>
2026-07-27 16:53:07 +0200

    Update ch02

--- delta_sigma_simulator/quantizer.py
+++ delta_sigma_simulator/quantizer.py
@@ -12,7 +12,7 @@ class Quantizer:
 
 
 class QuantizerDelayHysteresis(Quantizer):
-    def __init__(self, t_d, v_s=0.0):
+    def __init__(self, t_d=0.0, v_s=0.0):
         # Delay
         self.t_d = t_d
         # Threshold
@@ -23,7 +23,10 @@ class QuantizerDelayHysteresis(Quantizer):
         self.t_step = 10e-12
 
     def next(self, y):
-        y_v = lambda t: np.abs(y(t)[0]) - self.v_s
+        if self.v_s != 0.0:
+            y_v = lambda t: np.abs(y(t)[0]) - self.v_s
+        else:
+            y_v = lambda t: y(t)[0]
 
         dt = 2 * self.t_step
 
@@ -40,7 +43,17 @@ class QuantizerClock(Quantizer):
         self.f_c = f_c
         # Variance of the input noise
         self.n = n
+        # Current output value
+        self.v = +1.0
 
     def next(self, input_signal):
-        pass
- 
+        y_v = lambda t: input_signal(t)[0] + np.random.randn() * np.sqrt(self.n)
+
+        dt = 1 / self.f_c
+
+        while self.v * y_v(dt) > 0:
+            dt += 1 / self.f_c
+
+        self.v *= -1
+
+        return dt
